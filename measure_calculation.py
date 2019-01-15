37474a916e795c7a1fc17146cf1e24702482a8cc
Jongwan Kim <yena2bell@kaist.ac.kr>
2019-01-15 11:13:54 +0900

    change get_EC50, IC50 functions of measure_calculation.py. assuming the monotonicity of viability, modify for exception cases

--- measure_calculation.py
+++ measure_calculation.py
@@ -24,28 +24,33 @@ def adjust_viabilities(l_viabilities):
 def get_IC50(l_viabilities):#assume monotonic decrease of l_vialbilities
     f_threshold = 0.5
     
-    if l_viabilities[-1] >= 0.5:
+    if l_viabilities[-1] >= f_threshold:
         return 1
     
     i_num_points = len(l_viabilities)#contains drug concentration 0
     f_interval_drug_concen = 1.0/(i_num_points-1)
     for i in range(i_num_points-1):
-        if l_viabilities[i] == f_threshold:
-            return i*f_interval_drug_concen
-        if l_viabilities[i] > f_threshold and l_viabilities[i+1] < f_threshold:
-            return ((l_viabilities[i]-0.5)/(l_viabilities[i]-l_viabilities[i+1])+i)*f_interval_drug_concen#linear approximation
+        if l_viabilities[i] >= f_threshold and l_viabilities[i+1] < f_threshold:
+            return ((l_viabilities[i]-f_threshold)/(l_viabilities[i]-l_viabilities[i+1])+i)*f_interval_drug_concen#linear approximation
+        if l_viabilities[i] == l_viabilities[i+1] and l_viabilities[i] == f_threshold:
+            return (i+0.5)*f_interval_drug_concen
 
 
 def get_EC50(l_viabilities):
+    """assume l_viabilities are monotonic pattern."""
     f_threshold = (1 - l_viabilities[-1])/2.0 + l_viabilities[-1]
     
     i_num_points = len(l_viabilities)#contains drug concentration 0
     f_interval_drug_concen = 1.0/(i_num_points-1)
-    for i in range(i_num_points-1):
-        if l_viabilities[i] == f_threshold:
-            return i*f_interval_drug_concen
-        if l_viabilities[i] > f_threshold and l_viabilities[i+1] < f_threshold:
-            return ((l_viabilities[i]-f_threshold)/(l_viabilities[i]-l_viabilities[i+1])+i)*f_interval_drug_concen#linear approximation
+    
+    if l_viabilities[-1] >= f_threshold:
+        return 1
+    
+    for i in range(i_num_points,0,-1):
+        if l_viabilities[i-1] >= f_threshold and l_viabilities[i] < f_threshold:
+            return ((l_viabilities[i-1]-f_threshold)/(l_viabilities[i-1]-l_viabilities[i])+i-1)*f_interval_drug_concen#linear approximation
+        if l_viabilities[i-1] == l_viabilities[i] and l_viabilities[i] == f_threshold:
+            return (i-0.5)*f_interval_drug_concen
     
     return 1
 
